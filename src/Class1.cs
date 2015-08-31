b5762e73178af9bc5d99ff5e0367b8f62f659b81
Dave Gardner <dave@texasgardners.com>
2015-08-30 22:35:42 -0500

    Initial

---
+++ src/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salesforce.Migrations.Assistant
+{
+    public class Class1
+    {
+    }
+}
