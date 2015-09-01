cf7e035ad571129dc64a066ebe09884ee0c89916
Dave Gardner <dave@texasgardners.com>
2015-09-01 14:17:30 -0500

    made first set of git handlers and tests

---
+++ Salesforce.Migrations.Assistant.Library/GeneratePackageFile.cs
@@ -0,0 +1,70 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+
+
+namespace Salesforce.Migrations.Assistant.Library
+{
+    public static class GeneratePackageFile
+    {
+
+        public static void WithXMLOutput()
+        {
+            XmlOutput xo = new XmlOutput()
+                .XmlDeclaration()
+                .Node("package").Attribute("xmlns", "http://soap.sforce.com/2006/04/metadata").Within();
+                for (int i = 0; i < 10; i++ )
+                {
+                    xo.Node("types").Within()
+                        .Node("members").InnerText("SamplePage")
+                        .Node("name").InnerText("ApexPage")
+                        .EndWithin();
+                };
+                
+                xo.EndWithin()
+                .Node("version").InnerText("28.0");
+
+            xo.GetXmlDocument().Save("D:\\document.xml");
+        }
+
+        public static void Go()
+        {
+            //var ns = XNamespace.Get("http://soap.sforce.com/2006/04/metadata");
+
+            XmlDocument doc = new XmlDocument();
+
+            //(1) the xml declaration is recommended, but not mandatory
+            XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+          
+            XmlElement root = doc.DocumentElement;
+            doc.InsertBefore(xmlDeclaration, root);
+
+            string ns = "http://soap.sforce.com/2006/04/metadata";
+
+            //(2) string.Empty makes cleaner code
+            XmlElement element1 = doc.CreateElement(string.Empty, "package", ns);
+            doc.AppendChild(element1);
+
+            XmlElement element2 = doc.CreateElement(string.Empty, "types", ns);
+            element1.AppendChild(element2);
+
+            XmlElement element3 = doc.CreateElement(string.Empty, "members", ns);
+            XmlText text1 = doc.CreateTextNode("text");
+            element3.AppendChild(text1);
+            element2.AppendChild(element3);
+
+            XmlElement element4 = doc.CreateElement(string.Empty, "name", ns);
+            XmlText text2 = doc.CreateTextNode("other text");
+
+            XmlElement version = doc.CreateElement(string.Empty, "version", ns);
+            XmlText versionText = doc.CreateTextNode("28.0");
+            version.AppendChild(versionText);
+            element1.AppendChild(version);
+            
+
+            element4.AppendChild(text2);
+            element2.AppendChild(element4);
+
+            doc.Save("D:\\document.xml");
+        } 
+    }
+}
---
+++ src/Salesforce.Migrations.Assistant.Library.Tests/UnitTest1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using LibGit2Sharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Salesforce.Migrations.Assistant.Library.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var RFC2822Format = "ddd dd MMM HH:mm:ss yyyy K";
+            SMALGit git = new SMALGit(@"D:\code\SalesforceDevelopment\GitHub");
+
+            foreach (Commit c in git.Log())
+            {
+                Console.WriteLine("commit {0}", c.Id);
+
+                if (c.Parents.Count() > 1)
+                {
+                    Console.WriteLine("Merge: {0}",
+                        string.Join(" ", c.Parents.Select(p => p.Id.Sha.Substring(0, 7)).ToArray()));
+                }
+
+                Console.WriteLine("Author: {0} <{1}>", c.Author.Name, c.Author.Email);
+                Console.WriteLine("Date:   {0}", c.Author.When.ToString(RFC2822Format, CultureInfo.InvariantCulture));
+                Console.WriteLine();
+                Console.WriteLine(c.Message);
+                Console.WriteLine();
+            }
+        }
+
+        [Test]
+        public void GetDiffWithOnlySpecifyingCurrentCommit()
+        {
+            // arrange
+            SMALGit migrationassist = new SMALGit(@"D:\code\SalesforceDevelopment\GitHub", "c22310bd4e6f7a82060283ed63c9eccfb6621fa4");
+
+            // act
+            var listChanges = migrationassist.GetTreeChanges("origin/develop");
+
+            //assert
+            listChanges.ShouldNotBeEmpty();
+
+
+
+            using (var repo = new Repository(@"D:\code\SalesforceDevelopment\GitHub"))
+            {
+                foreach (TreeEntryChanges c in repo.Diff.Compare<TreeChanges>(repo.Head.Tip.Tree,
+                    DiffTargets.Index | DiffTargets.WorkingDirectory))
+                {
+                    Console.WriteLine(c);
+                }
+            }
+        }
+
+        [Test]
+        public void GetDiffTest2()
+        {
+            using (var repo = new Repository(@"D:\code\SalesforceDevelopment\GitHub"))
+            {
+                Tree headTree = repo.Head.Tip.Tree;
+
+                //Tree headTree2 = repo.Head.C;
+                var remoteBranches = repo.Branches.Where(p => p.IsRemote == true).ToList();
+
+
+                //foreach (var c in .Commits) { }
+                var branch = repo.Branches["origin/develop"];
+
+
+                var currentCommit = branch.Commits.FirstOrDefault();
+                if (currentCommit != null)
+                    Console.WriteLine("Current Commit == {0}-{1}", currentCommit.Id, currentCommit.Message);
+
+
+                var previousCommit = branch.Commits.Skip(1).FirstOrDefault();
+                if (previousCommit != null)
+                    Console.WriteLine("Previous Commit == {0}-{1}", previousCommit.Id, previousCommit.Message);
+
+
+                //Tree remoteMasterTree = repo.Branches["origin/develop"].Tip.Tree;
+
+                if (previousCommit != null && currentCommit != null)
+                {
+                    TreeChanges tc = repo.Diff.Compare<TreeChanges>(previousCommit.Tree, currentCommit.Tree);
+                    foreach (TreeEntryChanges c in tc)
+                    {
+                        Console.WriteLine(c.Path);
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void GetPackaging()
+        {
+             GeneratePackageFile.WithXMLOutput();
+
+        }
+}
+}
